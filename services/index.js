07af5fced9a3723150cde47649e564c27801c98f
TheMVProgrammer <franklin0415@hotmail.com>
2023-01-05 19:05:29 -0400

    Adding getRecentPosts request

--- services/index.js
+++ services/index.js
@@ -38,3 +38,24 @@ export const getPosts = async () => {
     return result.postsConnection.edges;
 
 }
+
+export const getRecentPosts = async () => {
+    const query = gql `
+        query getPostDetails() {
+            posts(
+                orderBy: createdAt_ASC  
+                last: 3                    
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query);
+    return result.posts;
+}
